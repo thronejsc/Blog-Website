b5358d38ab4d1af3653d53938bca9356dd8495b6
Elijah Joel S. Cañarejo <ejcanarejo21@gmail.com>
2024-02-20 16:26:23 +0800

    import request

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from datetime import date
-from flask import Flask, abort, render_template, redirect, url_for, flash
+from flask import Flask, abort, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
 from flask_gravatar import Gravatar
